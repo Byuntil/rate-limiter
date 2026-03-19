fa283215fedb62423c601263f27bdab11f704080
Byuntil <gugugu2014@jbnu.ac.kr>
2026-03-19 23:12:35 +0900

    feat(FixedWindowRateLimiter): 고정 시간 rate limiter 구현
    - 특정 시간(window) 동안 요청 개수를 세고, limit 넘으면 차단하는 방식

--- src/main/java/com/example/ratelimiter/ratelimit/algorithm/FixedWindowRateLimiter.java
+++ src/main/java/com/example/ratelimiter/ratelimit/algorithm/FixedWindowRateLimiter.java
@@ -1,10 +1,44 @@
 package com.example.ratelimiter.ratelimit.algorithm;
 
 import com.example.ratelimiter.ratelimit.RateLimitResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
-public class FixedWindowRateLimiter implements RateLimiter{
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Slf4j
+@Component
+public class FixedWindowRateLimiter implements RateLimiter {
+
+    private final ConcurrentHashMap<String, AtomicInteger> request = new ConcurrentHashMap<>();
+
     @Override
     public RateLimitResult tryAcquire(String key, int limit, int windowSeconds) {
-        return null;
+        String now = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmm"));
+        String value = key + ":" + now;
+
+        int count = request.computeIfAbsent(value, k -> new AtomicInteger(0)).incrementAndGet();
+
+        long resetAt = LocalDateTime.now()
+                .withSecond(0).withNano(0)
+                .plusMinutes(1)
+                .toEpochSecond(ZoneOffset.UTC);
+
+        if (count <= limit) {
+            return RateLimitResult.allowed(limit, limit - count, resetAt);
+        } else {
+            return RateLimitResult.blocked(limit, resetAt);
+        }
+    }
+
+    @Scheduled(fixedRate = 60_000)
+    public void cleanUp() {
+        String currentWindow = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmm"));
+        request.keySet().removeIf(k -> !k.contains(":" + currentWindow));
     }
 }
